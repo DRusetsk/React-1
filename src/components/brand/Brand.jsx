ef303e886d29770e7cc578fef723fb5116025107
Daniel <dukarus007@gmail.com>
2024-01-16 17:26:37 -0500

    Brand Section Complete

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './brand.css'
+import {cs, css, java, js, python, react} from "./imports"
+
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <p className='gradient__text'>My Experience:</p>
+      <div>
+        <img src={react} alt="react" />
+      </div>
+      <div>
+        <img src={cs} alt="cs" />
+      </div>
+      <div>
+        <img src={css} alt="css" />
+      </div>
+      <div>
+        <img src={java} alt="java" />
+      </div>
+      <div>
+        <img src={js} alt="js" />
+      </div>
+      <div>
+        <img src={python} alt="python" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,8 @@
+import cs from "../../assets/icons/cs.png";
+import css from "../../assets/icons/css.png";
+import java from "../../assets/icons/java.png";
+import js from "../../assets/icons/js.png";
+import python from "../../assets/icons/python.png";
+import react from "../../assets/icons/react.png";
+
+export {cs, css, java, js, python, react};
