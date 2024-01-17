872f7f40c5ada2b17b744f54ba7b32b20dcad38a
Daniel <dukarus007@gmail.com>
2024-01-17 17:35:02 -0500

    Fixed error

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,7 +4,7 @@ import './features.css'
 
 const Features = ({title,text}) => {
   return (
-    <div gpt3__features-container__feature>
+    <div className='gpt3__features-container__feature'>
       <div className='gpt3__features-container__feature-title'>
         <div />
         <h1>{title}</h1>
